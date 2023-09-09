354937593c1b7218b7ce1e2e591d754600740d71
Cyril-Wirth <cyril.wirth@gmail.com>
2023-09-09 23:41:49 +0900

    style: format

--- smart-page-spring/smart-page-spring-sql/src/main/java/io/narsha/smartpage/spring/sql/JdbcQueryParser.java
+++ smart-page-spring/smart-page-spring-sql/src/main/java/io/narsha/smartpage/spring/sql/JdbcQueryParser.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
-
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 
