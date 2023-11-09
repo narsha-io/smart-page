29f72e2eb72a5f768d4de7d47daa406256659286
narsha-io <narsha.io.pro@gmail.com>
2023-11-09 12:07:23 +0900

    build: update script

--- scripts/release-new-minor-version.sh
+++ scripts/release-new-minor-version.sh
@@ -5,14 +5,8 @@ CURRENT_FOLDER="$(dirname "$0")"
 echo "CWI pre-proc" \
 && ./mvnw -f $CURRENT_FOLDER/../pom.xml clean install \
 && echo "CWI clean" \
-&& ./mvnw -f $CURRENT_FOLDER/../pom.xml clean install -Prelease --batch-mode release:clean \
-&& echo "CWI install" \
-&& ./mvnw -f $CURRENT_FOLDER/../pom.xml clean install -pl smart-page-test,smart-page-spring/smart-page-spring-test -am \
+&& ./mvnw -f $CURRENT_FOLDER/../pom.xml -Prelease --batch-mode release:clean \
 && echo "CWI prepare" \
-&& ./mvnw -f $CURRENT_FOLDER/../pom.xml clean install -Prelease --batch-mode release:prepare \
-&& echo "CWI install 2" \
-&& ./mvnw -f $CURRENT_FOLDER/../pom.xml clean install -pl smart-page-test,smart-page-spring/smart-page-spring-test -am \
-&& echo "CWI install 3" \
-&& ./mvnw -f $CURRENT_FOLDER/../pom.xml clean install  \
-&& echo "CWI deploy" \
+&& ./mvnw -f $CURRENT_FOLDER/../pom.xml -Prelease --batch-mode release:prepare \
+&& echo "CWI perform" \
 && ./mvnw -f $CURRENT_FOLDER/../pom.xml verify -Prelease --batch-mode release:perform
