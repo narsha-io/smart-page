a41b6e1d25ec18e18ddb06c6c21336fd2ab6fee7
narsha-io <narsha.io.pro@gmail.com>
2023-11-09 12:17:13 +0900

    build: update script

--- scripts/release-new-minor-version.sh
+++ scripts/release-new-minor-version.sh
@@ -8,5 +8,7 @@ echo "CWI pre-proc" \
 && ./mvnw -f $CURRENT_FOLDER/../pom.xml -Prelease --batch-mode release:clean \
 && echo "CWI prepare" \
 && ./mvnw -f $CURRENT_FOLDER/../pom.xml -Prelease --batch-mode release:prepare \
+; ./mvnw -f $CURRENT_FOLDER/../pom.xml clean install \
+; ./mvnw -f $CURRENT_FOLDER/../pom.xml -Prelease --batch-mode release:prepare \
 && echo "CWI perform" \
 && ./mvnw -f $CURRENT_FOLDER/../pom.xml verify -Prelease --batch-mode release:perform
