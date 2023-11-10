c6446c29c002968d0c09d560f892c4ac4895ad9e
narsha-io <narsha.io.pro@gmail.com>
2023-11-10 21:18:11 +0900

    chore: create sub module

---
+++ smart-page-quarkus/smart-page-quarkus-sql/src/main/java/io/narsha/smartpage/quarkus/sql/SqlDataTable.java
@@ -0,0 +1,19 @@
+package io.narsha.smartpage.quarkus.sql;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/** Use this interface on the java class you want to use as a smart-page data */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SqlDataTable {
+
+  /**
+   * sql query to execute
+   *
+   * @return sql query
+   */
+  String query();
+}
